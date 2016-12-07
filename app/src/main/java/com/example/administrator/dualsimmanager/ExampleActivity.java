54490e4b706627c820e7a1aab29d70037ea45d67
hexuan <3696301017@qq.com>
2016-12-08 00:01:05 +0800

    add five

--- app/src/main/java/com/example/administrator/dualsimmanager/ExampleActivity.java
+++ app/src/main/java/com/example/administrator/dualsimmanager/ExampleActivity.java
@@ -1,4 +1,4 @@
-package com.example.administrator.androiddualsimmanager;
+package com.example.administrator.dualsimmanager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
