ab437851a0e3a21c58f0be4ba0884609f68b2091
hexuan <3696301017@qq.com>
2016-12-07 22:55:05 +0800

    Add first

--- app/src/main/java/com/example/administrator/androiddualsimmanager/ExampleActivity.java
+++ app/src/main/java/com/example/administrator/androiddualsimmanager/ExampleActivity.java
@@ -1,7 +1,10 @@
 package com.example.administrator.androiddualsimmanager;
 
+import android.content.Context;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
+        import android.os.Bundle;
+import android.telephony.TelephonyManager;
+import android.widget.Button;
 
 public class ExampleActivity extends AppCompatActivity {
 
@@ -9,5 +12,8 @@ public class ExampleActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_example);
+        Button button;
+        TelephonyManager manager = (TelephonyManager)getSystemService(Context.TELEPHONY_SERVICE);
+        manager.getSubscriberId();
     }
 }
